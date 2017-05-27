bb23df55d44341eb619e6ccdfefcf85172d6954a
mjansrud <morten@founder.no>
2017-05-27 13:23:40 +0200

    Initial commit

---
+++ bot/server.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const jwt = require('express-jwt');
+const jwks = require('jwks-rsa');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+app.listen(3002);
+console.log('Welcome to mBot, listening on localhost:3002');
